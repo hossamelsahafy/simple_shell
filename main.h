e264546f573a193e4df9be77395abea057b98ceb
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-10-16 16:38:23 +0300

    header file

--- main.h
+++ main.h
@@ -11,6 +11,15 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/stat.h>
+
+typedef struct path_node
+{
+    char *dir;
+    struct p_node *next;
+} p_node_t;
+
+extern char **environ;
 
 char **p_l(char *s);
 
@@ -18,11 +27,11 @@ char c_b(char *token);
 
 int e_b(char **ts, char **env);
 
-void execute(char **ts, char **env);
-
 void handler(int sig);
 
 void pipes(char *i, char *cmd[MAX_C][MAX_A], int p);
+
+void execute(char **ts, char **env);
 
 int _strlen(const char *s);
 
@@ -30,9 +39,15 @@ char *_strcpy(char *dest, char *src);
 
 char *_strcat(char *dest, char *src);
 
-int _path(int argc, char **argv, char **env);
-
-void execute_pipes(char *cmd[MAX_C][MAX_A], int p);
-
 void loop(char **env);
+
+void exe_echo(char **ts);
+
+void exe_cat(char **ts);
+
+int path_handler(char *c, char **env);
+
+char *_strchr(char *s, char c);
+
+int _strcmp(char *s1, char *s2);
 #endif
