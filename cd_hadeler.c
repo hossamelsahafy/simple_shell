3f4d642460866e393e369306ed78744590df941b
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-10-20 21:41:50 +0300

    files

--- cd_hadeler.c
+++ cd_hadeler.c
@@ -12,14 +12,16 @@ int handle_cd(char **ts)
 {
 	char cwd[1024];
 	char *pth = ts[1];
+	char *o_pwd;
 
+	o_pwd = getenv("OLDPWD");
 	if (pth == NULL)
 	{
 		pth = getenv("HOME");
 	}
 	else if (strcmp(pth, "-") == 0)
 	{
-		pth = getenv("OLDPWD");
+		pth = o_pwd;
 	}
 	if (chdir(pth) != 0)
 	{
@@ -28,6 +30,7 @@ int handle_cd(char **ts)
 	}
 	if (getcwd(cwd, sizeof(cwd)) != NULL)
 	{
+		setenv("OLDPWD", o_pwd, 1);
 		setenv("PWD", cwd, 1);
 	}
 	else
