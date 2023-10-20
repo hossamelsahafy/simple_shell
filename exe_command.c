e7c58b0cfdd8502fb3e0c29cc84dd0005781c35f
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-10-20 16:31:30 +0300

    files

--- exe_command.c
+++ exe_command.c
@@ -12,7 +12,6 @@
 int exe_command(char **ts, char **env)
 {
 	pid_t pid;
-	int exit_status;
 	int status;
 
 	pid = fork();
@@ -21,18 +20,21 @@ int exe_command(char **ts, char **env)
 		if (execve(ts[0], ts, env) == -1)
 		{
 			perror("./shell");
-			exit(1);
+			exit(EXIT_FAILURE);
 		}
 	}
 	else if (pid > 0)
 	{
 		wait(&status);
-		exit_status = WEXITSTATUS(status);
+		if (WIFEXITED(status))
+		{
+			return WEXITSTATUS(status);
+		}
 	}
 	else
 	{
 		perror("error");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
-	return (exit_status);
+	return (EXIT_SUCCESS);
 }
